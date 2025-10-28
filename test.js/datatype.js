cc1d881501de453ee7fae2e17b2c6ad8a2eea3d0
Abdurrehman-prog <abdurrehmann202@gmail.com>
2025-10-29 03:04:22 +0500

    pushing

---
+++ test.js/datatype.js
@@ -0,0 +1,21 @@
+"use strict"  // treat all code like newer version
+
+//alert (3 + 3)  // this is nodejs,not browser
+
+console.log(3 
+    + 3); // coding like this does not look professional
+
+console.log("rehman");
+let state = null;
+let isloggedin = false;
+let age = 18
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof state);
---
+++ test.js/variable.js
@@ -0,0 +1,22 @@
+const accountId = 1122335
+/*
+cannot be changed
+*/
+let accountEmail = "rehman@MediaList.com"
+var accountPassword = "iamrehman"
+/*
+can be changed
+*/
+accountCity = "faisalabad"
+
+let accountState = "0"
+
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "freefire@noob.com"
+accountPassword = "freefire"
+accountCity = "noobpur"
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
