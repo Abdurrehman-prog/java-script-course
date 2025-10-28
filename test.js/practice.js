32a88843fe212ac31a81b54bf1321106352204b4
Abdurrehman-prog <abdurrehmann202@gmail.com>
2025-10-29 03:48:10 +0500

    new practice

---
+++ test.js/practice.js
@@ -0,0 +1,14 @@
+const accountId = 255526
+let accountName = "Abdurrehman"
+let accountPassword = "8899"
+let accountAge = "19"
+
+console.log(`${accountName} his id is ${accountId} and password is given as ${accountPassword} and age is also ${accountAge}`
+);
+
+let Name = "abdurrehman";
+const language = "javascript";
+let video = 5
+
+console.log(`my name is ${Name} and i watched ${video} videos of code with chai while learning ${language}`);
+
