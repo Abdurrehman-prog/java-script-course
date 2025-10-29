02fd4094ab5a620e61782d8c87bdcb55ef447321
Abdurrehman-prog <abdurrehmann202@gmail.com>
2025-10-30 03:51:13 +0500

    Update practice.js

--- test.js/practice.js
+++ test.js/practice.js
@@ -12,3 +12,49 @@ let video = 5
 
 console.log(`my name is ${Name} and i watched ${video} videos of code with chai while learning ${language}`);
 
+let myNumber = "786";
+
+let valueNumber = Number(myNumber)
+console.log(`${valueNumber} is now a number`);
+
+let myName = 12345;
+let stringmyName = String(myName)
+ console.log(`${stringmyName} is now a string`);
+
+let isOnline = false;
+// in this it is a number
+let numberisOnline = Number(isOnline);
+// now i changed it to number
+console.log(typeof numberisOnline)
+// this will tell the type of numberisOnline
+console.log(numberisOnline)
+
+let nothingHere = null;
+// in this the data type is null
+let stringHere = String(nothingHere);
+//in this it will change it into string
+console.log(typeof stringHere);
+//this will tell the data type of stringHere
+console.log(stringHere);
+
+let notAssigned;
+//in this it is undefined
+let booleanNot = Boolean(notAssigned)
+//now i assigned it as boolean and change its data type
+console.log(typeof booleanNot);
+//it will tell datatype
+console.log(booleanNot)
+
+let randomNumber = 42;
+// its data type is number now
+let stringNumber = String(randomNumber)
+//but now i convert it to string
+console.log(typeof stringNumber)
+//this will tell the datatype 
+console.log(stringNumber)
+ 
+
+
+ 
+
+
