309b1336cf6dd47aae90d7c3134df15b524e6f6e
Abdurrehman-prog <abdurrehmann202@gmail.com>
2025-10-29 23:26:01 +0500

    new day new learning

---
+++ test.js/conversionoperation.js
@@ -0,0 +1,34 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(score)
+
+let age = Number(score);
+// console.log(typeof score);
+// console.log(age);
+
+let r = "string"
+//  console.log(r)
+//  console.log(typeof r);
+ 
+
+ let valueinNumber = Number(r)
+//  console.log(typeof valueinNumber);
+//  console.log(valueinNumber);
+
+ // "33" => 33
+ // "33abc" => Nan "not a number"
+ // true => 1 ; false=> 0;
+
+ let isloggedin = ""
+  
+ let booleanisloggedin = Boolean(isloggedin)
+//  console.log(typeof booleanisloggedin);
+//  console.log(booleanisloggedin);
+ 
+ let somenumber = 33
+
+ let stringNumber = String(somenumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber)
+ 
