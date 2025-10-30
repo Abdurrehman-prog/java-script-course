2939fd8d6d5095fa28ece71be233bad046e96233
Abdurrehman-prog <abdurrehmann202@gmail.com>
2025-10-30 23:47:04 +0500

    postfix and prefix learning time

---
+++ test.js/operation.js
@@ -0,0 +1,27 @@
+let value = 5
+let negvalue = value
+// console.log(negvalue)
+
+// console.log(2/3)
+
+let str1 = "hello"
+let str2 = " rehman"
+
+let str3 = str1 + str2
+// console.log(str3)
+ 
+// console.log(1 + 2 + "5")
+
+let eng1 = "1"
+let numeng1 = Number(eng1)
+let eng2 = 5
+// console.log(numeng1 + eng2)
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2+2
+// console.log(num2)
+
+let gamecounter = 100
+++gamecounter;
+// console.log(gamecounter);
